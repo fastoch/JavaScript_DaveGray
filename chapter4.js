a757ed32159082537545eac36736418fe09fa0fb
Fastoch <89261095+fastoch@users.noreply.github.com>
2024-07-18 14:45:56 +0200

    Update chapter4.js

--- chapter4.js
+++ chapter4.js
@@ -1,4 +1,16 @@
 // Numbers
 
-const myNumber = 42;
-console.log(myNumber);
+const myInteger = 42;
+console.log(myInteger);
+
+const myFloat = 42.30;    // 42.3
+console.log(myFloat);
+
+console.log(32.0 === 32); // true
+console.log(32.02 === 32); // false
+
+const myString = "42";
+console.log(myString === myInteger); // false because the data types are different
+
+console.log(myString + 3); // "423"
+console.log(Number(myString) + 3); // 45

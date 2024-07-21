85b4845d40ea7dcf8b075dd104faa7e5fca8686d
fastoch <fastoch@ik.me>
2024-07-21 19:46:02 +0200

    chapter 10 @60min

--- js/chapter10.js
+++ js/chapter10.js
@@ -7,5 +7,6 @@ console.log(myBool);
 let myName = prompt("Please enter your name:");
 // if we click the Cancel button, we get a null value
 // To prevent this, we use ?? (the nullish coalescing operator)
-// The nullish coalescing operator returns its right-hand side operand when its left-hand side operand is null or undefined, 
+// The nullish coalescing operator returns its right-hand side operand when 
+// its left-hand side operand is null or undefined, 
 console.log(`Hello ${myName ?? "you! You didn't enter your name"}!`);

b05835740b6b74845e4bda140699aec4afe3243c
fastoch <fastoch@ik.me>
2024-07-24 21:31:23 +0200

    @63 user input

--- js/chapter10.js
+++ js/chapter10.js
@@ -1,12 +1,17 @@
 // User input
+
+// aleryt text box
 alert("Simple alert message, returns nothing");
 
+// confirm box
 let myBool = confirm("OK to retrun true\nCancel to return false");
 console.log(myBool); 
 
+// prompt box
 let myName = prompt("Please enter your name:");
-// if we click the Cancel button, we get a null value
-// To prevent this, we use ?? (the nullish coalescing operator)
-// The nullish coalescing operator returns its right-hand side operand when 
-// its left-hand side operand is null or undefined, 
-console.log(`Hello ${myName ?? "you! You didn't enter your name"}!`);
+// if the user entered something, log it, otherwise log a default message
+if (myName) {   
+  console.log(`Hello ${myName}!`);
+} else {       
+  console.log("You didn't enter your name.");
+}

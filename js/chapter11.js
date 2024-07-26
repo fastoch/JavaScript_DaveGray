6e7d171c0e4c2b9977e79ca83b4dd799a615f92b
fastoch <fastoch@ik.me>
2024-07-26 07:10:11 +0200

    @70 rock, paper, scissors

--- js/chapter11.js
+++ js/chapter11.js
@@ -1,1 +1,15 @@
 // Your first interactive game
+let playGame = confirm('Shall we play rock, paper, scissors?');
+if (playGame) {
+  let playerChoice = prompt('Enter your choice (rock, paper, or scissors):');
+  if (playerChoice) {
+    formattedChoice = playerChoice.trim().toLowerCase();
+    formattedChoice === "rock" || formattedChoice === "paper" || formattedChoice === "scissors" ? 
+    
+    : alert('Invalid choice. Please enter rock, paper, or scissors.');
+  } else {
+    alert('I guess you changed your mind. Maybe another time...');
+  }
+} else {
+  alert('Ok, maybe next time!');
+}

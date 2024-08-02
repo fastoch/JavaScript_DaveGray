924df28e46605a2910be0bc99c2663e3db823bde
fastoch <fastoch@ik.me>
2024-08-02 19:39:28 +0200

    @165 chifoumi refactored with functions

--- js/chapter16-2.js
+++ js/chapter16-2.js
@@ -2,7 +2,7 @@
 const initGame = () => {
   const startGame = confirm("Shall we play rock, paper, or scissors?");
   startGame ? playGame() : alert("Ok, maybe next time");
-}
+};
 
 // Game flow function
 const playGame = () => {
@@ -15,7 +15,36 @@ const playGame = () => {
     }
     if (!playerChoice) {
       decideNotToPlay();
-      break; // exit the while loop
+      break; // exit the while loop (the game loop)
+    }
+    playerChoice = evaluatePlayerChoice(playerChoice);
+    if (!playerChoice) {
+      invalidChoice();
+      continue; 
+    }
+    const computerChoice = getComputerChoice();
+    const result = determineWinner(playerChoice, computerChoice);
+    displayResult(result);
+
+    if (askToPlayAgain()) {
+      continue;
+    } else {
+      thanksForPlaying();
+      break;
     }
   }
-}
+};
+
+const getPlayerChoice = () => {
+  return prompt("Please enter rock, paper, or scissors.");
+};
+
+const formatPlayerChoice = (playerChoice) => {
+  if (playerChoice || playerChoice === "") {
+    return playerChoice.trim().toLowerCase();
+  } else {
+    return false;
+  }
+};
+
+

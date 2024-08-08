a7c79bb94e32a6e090274e9a06db02fa4fbc701c
fastoch <fastoch@ik.me>
2024-08-08 14:40:44 +0200

    @198 Classes

--- js/chapter18.js
+++ js/chapter18.js
@@ -1,10 +1,17 @@
 // Javascript Classes
+
+// A class is a blueprint for creating objects
 class Pizza {
-  constructor() {
-    this.size = "medium";
+  constructor(pizzaType, pizzaSize) {
+    this.type = pizzaType;
+    this.size = pizzaSize;
     this.crust = "original";
   }
   bake() {
-    console.log(`Baking a ${this.size} ${this.crust} crust pizza...`);
+    console.log(`Baking a ${this.size} ${this.type} ${this.crust} crust pizza...`);
   }
 }
+
+// Creating an object 
+const myPizza = new Pizza("pepperoni", "large");
+myPizza.bake();

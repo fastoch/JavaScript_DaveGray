9aa47bbc0ef5fe6c90bd42df8a1a78e21c9b471d
Fastoch <fastoch@ik.me>
2024-09-19 13:20:33 +0000

    @251 working with the DOM

--- js/chapter21.js
+++ js/chapter21.js
@@ -7,5 +7,10 @@
 const view1 = document.getElementById("view1");
 
 // another way to do that
-const view2 = document.querySelector("#view2");
+const view2 = document.querySelector("#view2"); // using # to indicate that it's an ID
+
+// A Single Page Application (SPA) is typically built with different views
+// Here's how we can change the style to show or hide the different views
+view1.style.display = "none";
+view2.style.display = "flexbox";
 

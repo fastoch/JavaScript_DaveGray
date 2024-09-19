5be9de2fa37235828e1906bbd6fa09a06e8e2dc2
Fastoch <fastoch@ik.me>
2024-09-19 15:02:59 +0000

    @251 working with the DOM

--- js/chapter21.js
+++ js/chapter21.js
@@ -12,5 +12,5 @@ const view2 = document.querySelector("#view2"); // using # to indicate that it's
 // A Single Page Application (SPA) is typically built with different views
 // Here's how we can change the style to show or hide the different views
 view1.style.display = "none";
-view2.style.display = "flexbox";
+view2.style.display = "flex";
 

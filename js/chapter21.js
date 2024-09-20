e4d3f6b7a2bf94f87b8d075a494a585e24b2e5a7
Fastoch <89261095+fastoch@users.noreply.github.com>
2024-09-20 14:08:03 +0200

    @265min working with the DOM

--- js/chapter21.js
+++ js/chapter21.js
@@ -28,3 +28,19 @@ const sameDivs = view1.getElementsByTagName("div");
 // select even divs
 const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
 
+// change the background color and the dimensions of all even divs
+for (let i = 0; i < evenDivs.length; i++) {
+    evenDivs[i].style.backgroundColor = "darkblue";
+    evenDivs[i].style.width = "200px";
+    evenDivs[i].style.height = "200px";
+}
+
+// changing text in the navbar
+const navText = document.querySelector("nav h1"); // select the h1 element inside of the navbar
+navText.textContent = "I am a navbar!";
+
+// we can do the same by selecting the navbar and then using the innerHTML property
+const navbar = document.querySelector("nav");
+navbar.innerHTML = `<h1>Hello!</h1> <p>This should align right.</p>`;
+// Note that the innerHTML value will overwrite the textContent we previously added
+// the paragraph should align right because I'm using the flexbox layout + justify-content: space-between

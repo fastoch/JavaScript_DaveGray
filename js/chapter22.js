6e3828807b41b951a212cb55a2f918c1adddd0b7
fastoch <fastoch@ik.me>
2024-09-21 14:47:45 +0200

    @285 Event Listeners - the defer keywor

--- js/chapter22.js
+++ js/chapter22.js
@@ -1,7 +1,8 @@
 // EVENT LISTENERS
 /*
 Before we can interact with the DOM, we need to ensure that the DOM has actually loaded into the browser.
-For that, we need to declare our script tag in the <head> of our HTML document.
+For that, when we declare our script in the <head> of our HTML document, we need to use the keyword defer.
 */
+<script defer src="js/main.js"></script>
 
-const view = document.querySelector("#view2");
+// now the page will be loaded before ou JavaScript code gets executed

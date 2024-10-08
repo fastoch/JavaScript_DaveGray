5363dfa10a714380bf1b309aea30b13b6468bdbf
Fastoch <89261095+fastoch@users.noreply.github.com>
2024-10-08 21:16:19 +0200

    @347 Modules - export & import

---
+++ js/chapter24-2.js
@@ -0,0 +1,4 @@
+// let's import the functions we declared in chapter24.js
+import playGuitar from "./chapter24.js";
+
+console.log(playGuitar());
--- js/chapter24.js
+++ js/chapter24.js
@@ -29,8 +29,19 @@ const plucking = () => {
 
 // we can now EXPORT these functions for use in another .js file
 
-// to export only one of them:
+// to export only one of them and make it the default export for the current file:
 export default playGuitar;
 
 // to export multiple functions:
 export { shredding, plucking };
+
+// we can also export functions inline, when we create them
+export function playPiano() {
+  return "Playing the piano!";
+};
+
+export const touching = () => {
+  return "touching the keys";
+};
+
+// now we will import these functions into chapter24-2.js

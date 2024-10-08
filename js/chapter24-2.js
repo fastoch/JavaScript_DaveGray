1d2d66f242eb60d6a549cf38fc057b342a5dc539
Fastoch <89261095+fastoch@users.noreply.github.com>
2024-10-08 21:18:45 +0200

    @347 Modules - export & import

--- js/chapter24-2.js
+++ js/chapter24-2.js
@@ -1,4 +1,6 @@
 // let's import the functions we declared in chapter24.js
 import playGuitar from "./chapter24.js";
+import { shredding, plucking } from "./chapter24.js";
 
 console.log(playGuitar());
+console.log(shredding);

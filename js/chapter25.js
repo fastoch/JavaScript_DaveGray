aa3bc6d773ce83ca228a906a1132d9e3e89f1121
Fastoch <89261095+fastoch@users.noreply.github.com>
2024-10-20 15:01:21 +0200

    @362 Higher order functions - forEach() and filter()

--- js/chapter25.js
+++ js/chapter25.js
@@ -1,2 +1,22 @@
 // Higher Order Functions
 
+// A function that does at least one of the following:
+// ** Takes one or more functions as an argument (parameter)
+// ** Returns a function as the result
+
+import { posts } from "./posts.js";
+
+// for each post in the array of objects named "posts", log it to the console
+posts.forEach((post) => {
+  console.log(post);
+});
+
+// notice that we didn't need to define an iterator, nor to increment it
+
+console.clear();
+
+const filteredPosts = posts.filter((post) => {
+  return post.userId === 4;
+});
+
+console.log(filteredPosts);

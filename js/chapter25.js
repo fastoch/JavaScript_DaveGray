4bcfc5839ecfa206865e3e45e2d85d615b38b04e
Fastoch <89261095+fastoch@users.noreply.github.com>
2024-10-25 09:41:43 +0200

    @362 Higher order functions

--- js/chapter25.js
+++ js/chapter25.js
@@ -10,13 +10,20 @@ import { posts } from "./posts.js";
 posts.forEach((post) => {
   console.log(post);
 });
-
 // notice that we didn't need to define an iterator, nor to increment it
 
 console.clear();
 
+// Return only posts where the userId is equal to 4
 const filteredPosts = posts.filter((post) => {
   return post.userId === 4;
 });
 
 console.log(filteredPosts);
+
+console.clear();
+
+// 
+const mapped = 
+
+// forEach(), filter(), and map() are examples of Higher Order Functions

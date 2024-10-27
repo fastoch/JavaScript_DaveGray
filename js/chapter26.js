fc3a6ff1e5aeca44bad9f44b3d5b190a94b64919
Fastoch <89261095+fastoch@users.noreply.github.com>
2024-10-27 10:58:00 +0100

    @368 callback hell example

--- js/chapter26.js
+++ js/chapter26.js
@@ -1,2 +1,26 @@
 // Fetch / Async / Await
 
+/* 
+Fetch API requires a discussion of:
+- Callbacks
+- Promises 
+- Thenables
+- Async/Await
+*/
+
+// Callbacks are just functions that are passed to other functions as parameters
+
+// syntax:
+function firstFunction(parameters, callback) {
+  // do stuff
+  callback();
+}
+
+// Callback functions allow us to call a function that in turn will call another function
+// It essentially triggers a chain of events that can become what we call "callback hell"
+
+// Here's an example of why mutliplying callbacks can become a problem
+
+
+
+// Promises are designed to get rid of this "callback hell" issue

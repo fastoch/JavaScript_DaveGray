d699ff1563343c0c9a921c5ad11107752122830b
Fastoch <89261095+fastoch@users.noreply.github.com>
2024-10-27 11:36:48 +0100

    @370 Promises

--- js/chapter26.js
+++ js/chapter26.js
@@ -8,7 +8,8 @@ Fetch API requires a discussion of:
 - Async/Await
 */
 
-// Callbacks are just functions that are passed to other functions as parameters
+
+// CALLBACKS are just functions that are passed to other functions as parameters
 
 // syntax:
 function firstFunction(parameters, callback) {
@@ -17,10 +18,31 @@ function firstFunction(parameters, callback) {
 }
 
 // Callback functions allow us to call a function that in turn will call another function
-// It essentially triggers a chain of events that can become what we call "callback hell"
+// It essentially triggers a chain of events that can quickly become "callback hell"
 
-// Here's an example of why mutliplying callbacks can become a problem
+firstFunction(param, function() {
+  // do stuff
+  secondFunction(param, function() {
+    // do stuff
+    thirdFunction(param, function() {
+
+    });
+  });
+});
+
+// The above structure makes your code hard to read and understand
+
+/* 
+PROMISES are designed to get rid of this "callback hell" issue
+Nowadays, we don't use callbacks anymore, but you probably could find callbacks 
+in some legacy code written before promises existed.
+*/
+
+// Promises can have 3 states: Pending, Fulfilled, Rejected
+/*
+Promises deliver ASYNC code (asynchronous).
+While JavaScript is usually SYNCHRONOUS, meaning doing one thing at a time.
 
 
+*/
 
-// Promises are designed to get rid of this "callback hell" issue
